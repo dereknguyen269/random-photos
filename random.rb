e27311f8db4d87304ac4186a70c03b86a76f28e7
Nguyen Quan <quanmn.libra@gmail.com>
2017-06-16 08:59:36 +0700

    Change background color

--- random.rb
+++ random.rb
@@ -2,8 +2,8 @@ require_relative 'dependencies'
 
 class Random
   def initialize
-    puts ColorizedString["Starting random photo \u{1f600} \u{1f600}  ..."].colorize(:color => :light_red, :background => :light_white)
-    puts ColorizedString["Please wait a little \u{1f609}  ..."].colorize(:color => :light_blue, :background => :light_white)
+    puts ColorizedString["Starting random photo \u{1f600} \u{1f600}  ..."].colorize(:color => :light_red, :background => :black)
+    puts ColorizedString["Please wait a little \u{1f609}  ..."].colorize(:color => :light_blue, :background => :black)
   end
 
   def run
@@ -17,6 +17,6 @@ class Random
 end
 
 random_photo = Random.new.run
-puts ColorizedString["Great Job \u{1f618}  .You can visit this url:"].colorize(:color => :green, :background => :light_white)
-puts ColorizedString["#{random_photo}"].colorize(:color => :light_red, :background => :light_white)
+puts ColorizedString["Great Job \u{1f618}  .You can visit this url:"].colorize(:color => :green, :background => :black)
+puts ColorizedString["#{random_photo}"].colorize(:color => :light_red, :background => :black)
 Launchy.open(random_photo)
